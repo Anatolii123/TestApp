2ce7d57c13276dbac4cc575b951773bb0cb7cc92
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-26 18:04:04 +0500

    TestApp2Commit

--- src/main/java/dao/PersonCarDao.java
+++ src/main/java/dao/PersonCarDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import entities.Car;
+import entities.Person;
+import entities.PersonWithCars;
+import entities.Statistics;
+
 public interface PersonCarDao {
+
+    void savePerson(Person person);
+
+    void saveCar(Car car);
+
+    PersonWithCars showPersonsCars(String email, String password);
+
+    int createPersonId();
+
+    int createCarId();
+
+    void clear();
+
+    Statistics getStatistics();
 }
---
+++ src/main/java/dao/PersonCarDaoImpl.java
@@ -0,0 +1,79 @@
+package dao;
+
+import entities.Car;
+import entities.Person;
+import entities.PersonWithCars;
+import entities.Statistics;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Projections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class PersonCarDaoImpl implements PersonCarDao {
+
+    public JdbcTemplate jdbcTemplate;
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    @Autowired
+    public PersonCarDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void savePerson(Person person) {
+
+    }
+
+    @Override
+    public void saveCar(Car car) {
+
+    }
+
+    @Override
+    public PersonWithCars showPersonsCars(String email, String password) {
+        return null;
+    }
+
+    @Override
+    public int createPersonId() {
+        Criteria criteria = getSession().createCriteria(Person.class).setProjection(Projections.max("id"));
+        int newId = (int) criteria.uniqueResult() + 1;
+        return newId;
+    }
+
+    @Override
+    public int createCarId() {
+        Criteria criteria = getSession().createCriteria(Car.class).setProjection(Projections.max("id"));
+        int newId = (int) criteria.uniqueResult() + 1;
+        return newId;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Statistics getStatistics() {
+        return null;
+    }
+}
