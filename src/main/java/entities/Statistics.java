db4e2b6d26ae06874348f509f93c1b5e732f40cb
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-26 18:27:06 +0500

    TestApp7Commit

--- src/main/java/entities/Statistics.java
+++ src/main/java/entities/Statistics.java
@@ -1,4 +1,32 @@
 package entities;
 
 public class Statistics {
+
+    private Long personcount;
+    private Long carcount;
+    private Long uniquevendercount;
+
+    public Long getPersoncount() {
+        return personcount;
+    }
+
+    public void setPersoncount(Long personcount) {
+        this.personcount = personcount;
+    }
+
+    public Long getCarcount() {
+        return carcount;
+    }
+
+    public void setCarcount(Long carcount) {
+        this.carcount = carcount;
+    }
+
+    public Long getUniquevendercount() {
+        return uniquevendercount;
+    }
+
+    public void setUniquevendercount(Long uniquevendercount) {
+        this.uniquevendercount = uniquevendercount;
+    }
 }
