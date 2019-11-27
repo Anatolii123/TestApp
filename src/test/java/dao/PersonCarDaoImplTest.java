952f95cbd6bb9c5f306bef34256b097657c43ff5
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-27 11:24:47 +0500

    TestApp21Commit

---
+++ src/test/java/dao/PersonCarDaoImplTest.java
@@ -0,0 +1,40 @@
+package dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonCarDaoImplTest {
+
+    @Test
+    public void getSession() {
+    }
+
+    @Test
+    public void savePerson() {
+    }
+
+    @Test
+    public void saveCar() {
+    }
+
+    @Test
+    public void showPersonsCars() {
+    }
+
+    @Test
+    public void createPersonId() {
+    }
+
+    @Test
+    public void createCarId() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void getStatistics() {
+    }
+}
