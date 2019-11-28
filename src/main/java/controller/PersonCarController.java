6bee19c2e5be8edee566eac7a68406e327513725
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-28 14:39:22 +0500

    TestApp38Commit

--- src/main/java/controller/PersonCarController.java
+++ src/main/java/controller/PersonCarController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import dao.PersonCarDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.PersonCarService;
+
+import java.util.Date;
+
+@RestController
+@CrossOrigin(value = "*")
+@RequestMapping("/")
 public class PersonCarController {
+
+    @Autowired
+    public PersonCarService personCarService;
+
+    @Autowired
+    public PersonCarDao personCarDao;
+
+
+
 }
