ae735bbf55e6e4b7fe38604eb49259159ed3903b
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-28 14:20:24 +0500

    TestApp34Commit

--- src/main/java/service/PersonCarService.java
+++ src/main/java/service/PersonCarService.java
@@ -1,4 +1,16 @@
 package service;
 
+import entities.Car;
+import entities.Person;
+import entities.PersonWithCars;
+import entities.Statistics;
+
 public interface PersonCarService {
+    void savePerson(Person person);
+
+    void saveCar(Car car);
+
+    PersonWithCars showPersonsCars(long personId);
+
+    Statistics getStatistics();
 }
