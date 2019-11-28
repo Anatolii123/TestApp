3bcf7a6e6e8d6366266c842ed7c77326e882e5aa
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-28 15:57:09 +0500

    TestApp42Commit

--- src/main/java/controller/PersonCarController.java
+++ src/main/java/controller/PersonCarController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import dao.PersonCarDao;
+import entities.Car;
 import entities.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,16 @@ public class PersonCarController {
     @PostMapping(value = "/person")
     public void savePerson(@RequestBody Person person) {
         personCarService.savePerson(person);
+    }
+
+    @PostMapping(value = "/car")
+    public void saveCar(@RequestBody Car car) {
+        personCarService.saveCar(car);
+    }
+
+    @GetMapping(value = "/personwithcars", params = {"personId"})
+    public void getPersonWithCars(@RequestParam("personId") Long personId) {
+
     }
 
 }
