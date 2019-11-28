df371aede83436b1565b0171b89d97fdd47bcb02
AKusakin <anatolii.kusakin1234@gmail.com>
2019-11-28 16:08:43 +0500

    TestApp45Commit

---
+++ src/main/java/controller/Person.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.util.Date;
+
+public class Person {
+    private long id;
+    private String name;
+    private Date birthdate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+}
